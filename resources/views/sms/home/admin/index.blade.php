85c0fa196ba608bc15211e9312cc0b7377bfee6f
monty725 <62430156+Monty725@users.noreply.github.com>
2025-09-17 09:33:49 +0800

    CY UPDATE

--- resources/views/sms/home/admin/index.blade.php
+++ resources/views/sms/home/admin/index.blade.php
@@ -4,7 +4,8 @@
 
     <section class="content-header">
         <h1>
-            CY: {{$cy->name}}
+{{--            CY: {{$cy->name}}--}}
+            CY: 2025-2026
             <span class="pull-right"><small>Current week ending:</small> {{\Illuminate\Support\Carbon::parse($closestSundayAhead)->format('F d, Y')}}</span>
         </h1>
     </section>
