a1b005e1c534a424d4224aecb62542d830c2fad8
monty725 <62430156+Monty725@users.noreply.github.com>
2025-10-08 10:09:00 +0800

    Form2 fix thisweek and prevweek

--- app/Http/Controllers/GetForms/GetForm2Controller.php
+++ app/Http/Controllers/GetForms/GetForm2Controller.php
@@ -670,7 +670,8 @@ class GetForm2Controller extends Controller
             ->leftJoin('weekly_reports','weekly_reports.slug','=','form5a_deliveries.weekly_report_slug')
             ->where('crop_year','=',$weeklyReport->crop_year)
             ->where('mill_code','=',$weeklyReport->mill_code)
-            ->where('report_no','<=', $reportNo != 0 ? $reportNo : $weeklyReport->report_no * 1)
+//            ->where('report_no','<=', $reportNo != 0 ? $reportNo : $weeklyReport->report_no * 1)
+            ->where('report_no','<=', $reportNo)
             ->where(function($q){
                 $q->where('weekly_reports.status' ,'!=', -1)
                     ->orWhere('weekly_reports.status', '=', null);
@@ -688,7 +689,8 @@ class GetForm2Controller extends Controller
             ->whereNotNull('rsq_no')
             ->where('crop_year','=',$weeklyReport->crop_year)
             ->where('mill_code','=',$weeklyReport->mill_code)
-            ->where('report_no','<=', $reportNo != 0 ? $reportNo : $weeklyReport->report_no * 1)
+//            ->where('report_no','<=', $reportNo != 0 ? $reportNo : $weeklyReport->report_no * 1)
+            ->where('report_no','<=', $reportNo)
             ->where(function($q){
                 $q->where('weekly_reports.status' ,'!=', -1)
                     ->orWhere('weekly_reports.status', '=', null);
